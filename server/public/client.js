f38803568aaa67491e3fc37febfead02fd124d82
arkuar <arttukuronen@gmail.com>
2020-12-07 02:23:53 +0200

    Add login for users
    - Record active users in redis
    - Refactor index page with login view

--- server/public/client.js
+++ server/public/client.js
@@ -1,10 +1,15 @@
 const formEl = document.querySelector('#message-form')
 const messageList = document.querySelector('#messages')
+const loginEl = document.querySelector('#login')
+const chatEl = document.querySelector('#chat')
+const errorEl = document.querySelector('#error')
 
+const url = window.location.host
 let ws
+let user
 
 function connect() {
-    const socket = new WebSocket('ws://localhost:8080')
+    const socket = new WebSocket(`ws://${url}/?user=${user}`)
 
     socket.onopen = () => {
         console.log('Opened connection')
@@ -12,25 +17,45 @@ function connect() {
     
     socket.onmessage = (event) => {
         console.log(`Received message ${event.data}`)
-        const item = document.createElement('li')
-        const autoScroll = messageList.scrollTop + messageList.clientHeight === messageList.scrollHeight
-    
-        item.appendChild(document.createTextNode(event.data))
-        messageList.appendChild(item)
-    
-        if (autoScroll) {
-            messageList.scrollTop = messageList.scrollHeight
-        }
+        showMessage(event.data)
     }
     
     socket.onclose = () => {
         console.log('Connection was closed, try reconnecting')
         setTimeout(() => {
-            ws = connect()
+            connect()
         }, 5000)
     }
 
-    return socket
+    ws = socket
+}
+
+function showMessage(message) {
+    const item = document.createElement('li')
+    const autoScroll = messageList.scrollTop + messageList.clientHeight === messageList.scrollHeight
+    item.appendChild(document.createTextNode(message))
+    messageList.appendChild(item)
+    if (autoScroll) {
+        messageList.scrollTop = messageList.scrollHeight
+    }
+}
+
+function handleLoginResponse(response) {
+    return response.json().then(({ userName, message }) => {
+        if(response.ok) {
+            // Show chat and hide login view
+            chatEl.style.display = 'block'
+            loginEl.style.display = 'none'
+            user = userName
+            return message
+        }
+        throw new Error(message)
+     })
+}
+
+function showError(error) {
+    errorEl.style.display = 'block'
+    errorEl.textContent = error.message
 }
 
 formEl.addEventListener('submit', (event) => {
@@ -40,5 +65,21 @@ formEl.addEventListener('submit', (event) => {
     ws.send(message)
 })
 
-ws = connect()
+loginEl.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    fetch(`http://${url}/login`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userName })
+        })
+        .then(handleLoginResponse)
+        .then(showMessage)
+        .then(connect)
+        .catch(showError)
+})
 
+chatEl.style.display = 'none'
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const WebSocket = require('ws')
 const redis = require('redis')
 const fs = require('fs')
+
 const app = express();
 
 // ENV variables
@@ -13,25 +14,56 @@ const NAME = process.env.NAME || 'server'
 const subscriber = redis.createClient({ host: REDIS_HOST })
 const publisher = redis.createClient({ host: REDIS_HOST })
 
+// Redis client for regular operations
+const redisClient = redis.createClient({host: REDIS_HOST})
+
 // Log path variables, initialized after server has started
 let logPath
 
 // Subscribe to messages so they can be broadcasted to all connected client sockets
 subscriber.on('message', (channel, message) => {
     log(`Received message from channel ${channel}. Message content: ${message}`)
+    // Handle special cases when user joins or leaves
+    switch (channel) {
+        case 'users:join':
+            message = `${message} joined`
+            break
+        case 'users:leave':
+            message = `${message} left`
+            break
+    }
     broadcast(message)
 })
 
 subscriber.on('error', redisError)
-
 publisher.on('error', redisError)
+redisClient.on('error', redisError)
 
 subscriber.subscribe('chat:messages')
+subscriber.subscribe('users:join')
+subscriber.subscribe('users:leave')
 
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/ping', (req, res) => {
     res.send('pong')
+})
+
+app.post('/login', (req, res) => {
+    const { userName } = req.body
+    
+    // Check if username is already taken
+    redisClient.sismember('users', userName, (_, reply) => {
+        if (reply) {
+            log(`Username ${userName} already in use, denying login`)
+            res.status(409).send({message: 'Username already in use'})
+        } else {
+            redisClient.sadd('users', userName)
+            log(`Updating session for user ${userName}`)
+            res.status(200).send({ userName, message: 'Login succesful' })
+        }
+    })
 })
 
 const server = app.listen(PORT, () => {
@@ -39,7 +71,7 @@ const server = app.listen(PORT, () => {
     log(`Application listening on port ${PORT}`)
 })
 
-const ws = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ noServer: true })
 
 /**
  * Error handler for redis clients
@@ -52,7 +84,7 @@ function redisError(error) {
  * Logger function that writes the message to file and prints it to stdout
  */
 function log(message) {
-    fs.appendFile(logPath, `${new Date().toISOString()}\t${message}\n`, (err) => {
+    fs.appendFile(logPath, `[${NAME}]\t${new Date().toISOString()}\t${message}\n`, (err) => {
         if (err) throw err
     })
     console.log(message)
@@ -63,25 +95,48 @@ function log(message) {
  * @param {string} message
  */
 function broadcast(message) {
-    ws.clients.forEach((client) => {
+    wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(message);
         }
     });
 }
 
-ws.on('connection', (socket) => {
-    console.log(`Client connected`)
-    socket.isAlive = true
+/**
+ * Establish websocket connection with the client
+ */
+server.on('upgrade', (request, socket, head) => {
+    const [query, user] = request.url?.split('=')
+    if (query !== '/?user' || !user) {
+        socket.destroy(new Error('Username missing or it is invalid'))
+        return
+    }
+    // Check if user is logged in
+    redisClient.sismember('users', user, (err, reply) => {
+        if (!reply) {
+            socket.destroy(new Error('Login to use the websocket'))
+            return
+        }
+        request.session = { user }
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request)
+        })
+    })
+})
 
-    socket.on('pong', () => { socket.isAlive = true })
+wss.on('connection', (socket, request) => {
+    let { user } = request.session
+    log(`User ${user} connected`)
+    publisher.publish('users:join', user)
 
     socket.on('message', (message) => {
-        log(`Publishing message: ${message}`)
-        publisher.publish('chat:messages', message)
+        log(`User ${user} sent message: ${message}`)
+        publisher.publish('chat:messages', `${user}: ${message}`)
     })
 
     socket.on('close', () => {
-        log('Client disconnected')
+        redisClient.srem('users', user) // Free username for use
+        publisher.publish('users:leave', user)
+        log(`User ${user} disconnected`)
     })
 })
